f7a4f5f889e00d2953e7c22db4d98d6b98348c4a
Muhammad Shaharyar <muhammadshaharyaraulakh@gmail.com>
2025-10-19 11:13:07 +0500

    push files

---
+++ 24-Number_Guess/main.js
@@ -0,0 +1,42 @@
+let number = document.getElementById("guessInput");
+let Checker = document.getElementById("guessBtn");
+let message = document.getElementById("messageArea");
+let RealNumber = Math.floor(Math.random() * 100 + 1);
+Checker.addEventListener("click", () => {
+    let userNumber = parseInt(number.value);
+    if (isNaN(userNumber) || number.value === "" || userNumber > 100) {
+        message.innerText = "Please enter Suitable Number";
+        setTimeout(() => {
+            message.innerText = "";
+        }, 1000);
+        return;
+    }
+    if (RealNumber === userNumber) {
+        let messageTimer = setInterval(() => {
+            message.innerText = "You Guessed It";
+        }, 100);
+        setTimeout(() => {
+            clearInterval(messageTimer);
+            message.innerText = "";
+            number.value = "";
+        }, 1000);
+    } else if (RealNumber > userNumber) {
+        let messageTimer = setInterval(() => {
+            message.innerText = "Low";
+        }, 100);
+        setTimeout(() => {
+            clearInterval(messageTimer);
+            message.innerText = "";
+        }, 1000);
+        number.value = "";
+    } else if (RealNumber < userNumber) {
+        let messageTimer = setInterval(() => {
+            message.innerText = "High";
+        }, 100);
+        setTimeout(() => {
+            clearInterval(messageTimer);
+            message.innerText = "";
+            number.value = "";
+        }, 1000);
+    }
+});
